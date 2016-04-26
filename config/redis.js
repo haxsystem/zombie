ecf684ef2d6d70f6173d5ac4b4d4af3b69c6ae33
bmonsalvevera <bmonsalvevera@groupon.com>
2016-04-26 13:53:08 -0300

    Remove model

--- config/redis.js
+++ config/redis.js
@@ -1,14 +1,10 @@
-var caminte = require('caminte');
-var driver = 'redis';
-
 var redis = {
   local: {
-    driver: driver,
     host: "localhost",
     port: "6379",
     user: '',
     password: '',
-    database: "zombie_manager"
+    keys: {}
   },
   development: {},
   uat: {},
@@ -16,11 +12,23 @@ var redis = {
 }
 
 module.exports = function(environment) {
-  var Caminte = caminte.Schema;
-  var schema = new Caminte(redis[environment].driver, redis[environment]);
-  var config = {
-    schema: schema
-  };
-
-  return config;
+  var response = null;
+  switch (environment) {
+    case 'local':
+      response = redis['local'];
+      break;
+    case 'development':
+      response = redis['development'];
+      break;
+    case 'uat':
+      response = redis['uat'];
+      break;
+    case 'production':
+      response = redis['production'];
+      break;
+    default:
+      response = redis['production'];
+      break;
+  }
+  return response;
 }
